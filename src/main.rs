5e0b7f689868fd0c29f20a712031e28fceffffda
Pirmin Kalberer <pka@sourcepole.ch>
2019-09-23 23:29:02 +0200

    Update tokio-postgres to std-futures branch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use futures::{Future, Stream};
+use futures::executor::block_on;
+use futures::{FutureExt, TryStreamExt};
 use nu::{
     serve_plugin, CallInfo, Plugin, Primitive, ReturnSuccess, ReturnValue, ShellError, Signature,
     Tagged, Value,
 };
-use tokio_postgres::NoTls;
+use tokio_postgres::{Error, NoTls, Row};
 
 struct Psql;
 
@@ -15,7 +16,8 @@ impl Psql {
     fn cmd(&mut self, value: Tagged<Value>) -> Result<Tagged<Value>, ShellError> {
         match value.item {
             Value::Primitive(Primitive::String(_s)) => Ok(Tagged {
-                item: self.psql("host=localhost user=postgres".to_string()),
+                item: block_on(self.psql("host=localhost user=postgres".to_string()))
+                    .map_err(|e| ShellError::string(format!("{}", e)))?,
                 tag: value.tag,
             }),
             _ => Err(ShellError::labeled_error(
@@ -26,52 +28,36 @@ impl Psql {
         }
     }
 
-    fn psql(&mut self, connstr: String) -> Value {
-        let fut =
-        // Connect to the database
-        tokio_postgres::connect(&connstr, NoTls)
+    async fn psql(&mut self, connstr: String) -> Result<Value, Error> {
+        // Connect to the database.
+        let (mut client, connection) = tokio_postgres::connect(&connstr, NoTls).await?;
 
-        .map(|(client, connection)| {
-            // The connection object performs the actual communication with the database,
-            // so spawn it off to run on its own.
-            let connection = connection.map_err(|e| eprintln!("connection error: {}", e));
-            tokio::spawn(connection);
+        // The connection object performs the actual communication with the database,
+        // so spawn it off to run on its own.
+        let connection = connection.map(|r| {
+            if let Err(e) = r {
+                eprintln!("connection error: {}", e);
+            }
+        });
+        tokio::spawn(connection);
 
-            // The client is what you use to make requests.
-            client
-        })
+        // Now we can prepare a simple statement that just returns its parameter.
+        let stmt = client.prepare("SELECT $1::TEXT").await?;
 
-        .and_then(|mut client| {
-            // Now we can prepare a simple statement that just returns its parameter.
-            client.prepare("SELECT $1::TEXT")
-                .map(|statement| (client, statement))
-        })
-
-        .and_then(|(mut client, statement)| {
-            // And then execute it, returning a Stream of Rows which we collect into a Vec
-            client.query(&statement, &[&"hello world"]).collect()
-        })
+        // And then execute it, returning a Stream of Rows which we collect into a Vec.
+        let rows: Vec<Row> = client.query(&stmt, &[&"hello world"]).try_collect().await?;
 
         // Now we can check that we got back the same string we sent over.
-        .map(|rows| {
-            let value: &str = rows[0].get(0);
-            Value::string(value)
-        })
-
-        // And report any errors that happened.
-        .map_err(|e| {
-            eprintln!("error: {}", e);
-        });
-
-        // By default, tokio_postgres uses the tokio crate as its runtime.
-        let mut runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-        runtime.block_on(fut).unwrap()
+        let value: &str = rows[0].get(0);
+        Ok(Value::string(value))
     }
 }
 
 impl Plugin for Psql {
     fn config(&mut self) -> Result<Signature, ShellError> {
-        Ok(Signature::build("psql").desc("Execute PostgreSQL query."))
+        Ok(Signature::build("psql")
+            .desc("Execute PostgreSQL query.")
+            .filter())
     }
 
     fn begin_filter(&mut self, _: CallInfo) -> Result<Vec<ReturnValue>, ShellError> {
@@ -83,6 +69,7 @@ impl Plugin for Psql {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     serve_plugin(&mut Psql::new());
 }
