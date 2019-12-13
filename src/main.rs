7ce969378651b1a78b9cdf19dae2356de39ab61c
Pirmin Kalberer <pka@sourcepole.ch>
2019-12-13 22:51:16 +0100

    WIP: Update to stable Rust and latest crates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use futures::executor::block_on;
-use futures::{FutureExt, TryStreamExt};
-use nu::{
-    serve_plugin, CallInfo, Plugin, Primitive, ReturnSuccess, ReturnValue, ShellError, Signature,
-    SyntaxShape, Tag, Tagged, TaggedDictBuilder, Value,
+use nu_errors::ShellError;
+use nu_protocol::{
+    serve_plugin, CallInfo, Plugin, Primitive, ReturnSuccess, ReturnValue, Signature, SyntaxShape,
+    TaggedDictBuilder, UntaggedValue, Value,
 };
+use nu_source::Tag;
 use tokio_postgres::{types::Type, Error, NoTls, Row};
 
 struct Psql {
@@ -19,49 +20,51 @@ impl Psql {
         }
     }
 
-    fn cmd(&mut self, tag: Tag) -> Result<Vec<Tagged<Value>>, ShellError> {
+    fn cmd(&mut self, tag: Tag) -> Result<Vec<Value>, ShellError> {
         block_on(psql(
             self.conn.as_ref().unwrap(),
             self.query.as_ref().unwrap(),
             tag,
         ))
-        .map_err(|e| ShellError::string(format!("{}", e)))
+        .map_err(|e| ShellError::untagged_runtime_error(format!("{}", e)))
     }
 }
 
-async fn psql(connstr: &str, query: &str, tag: Tag) -> Result<Vec<Tagged<Value>>, Error> {
-    let (mut client, connection) = tokio_postgres::connect(&connstr, NoTls).await?;
-    let connection = connection.map(|r| {
-        if let Err(e) = r {
+async fn psql(connstr: &str, query: &str, tag: Tag) -> Result<Vec<Value>, Error> {
+    let (client, connection) = tokio_postgres::connect(&connstr, NoTls).await?;
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
             eprintln!("connection error: {}", e);
         }
     });
-    tokio::spawn(connection);
-
     let stmt = client.prepare(query).await?;
     let columns = stmt.columns();
 
     let mut records = vec![];
-    let rows: Vec<Row> = client.query(&stmt, &[]).try_collect().await?;
+    let rows: Vec<Row> = client.query(&stmt, &[]).await?;
     for row in rows {
-        let mut dict = TaggedDictBuilder::new(tag);
+        let mut dict = TaggedDictBuilder::new(&tag);
         for (i, col) in columns.iter().enumerate() {
             let opt_value = match col.type_() {
-                &Type::TEXT | &Type::VARCHAR => row.try_get::<_, &str>(i).map(Value::string),
-                &Type::INT2 => row.try_get::<_, i16>(i).map(Value::int),
-                &Type::INT4 => row.try_get::<_, i32>(i).map(Value::int),
-                &Type::INT8 => row.try_get::<_, i64>(i).map(Value::int),
-                &Type::FLOAT4 => row.try_get::<_, f32>(i).map(Value::decimal),
-                &Type::FLOAT8 => row.try_get::<_, f64>(i).map(Value::decimal),
-                // &Type::NUMERIC => row.try_get::<_, f64>(i).map(Value::decimal),
-                &Type::BOOL => row.try_get::<_, bool>(i).map(Value::boolean),
+                &Type::TEXT | &Type::VARCHAR => {
+                    row.try_get::<_, &str>(i).map(UntaggedValue::string)
+                }
+                &Type::INT2 => row.try_get::<_, i16>(i).map(UntaggedValue::int),
+                &Type::INT4 => row.try_get::<_, i32>(i).map(UntaggedValue::int),
+                &Type::INT8 => row.try_get::<_, i64>(i).map(UntaggedValue::int),
+                &Type::FLOAT4 => row.try_get::<_, f32>(i).map(UntaggedValue::decimal),
+                &Type::FLOAT8 => row.try_get::<_, f64>(i).map(UntaggedValue::decimal),
+                // &Type::NUMERIC => row.try_get::<_, f64>(i).map(UntaggedValue::decimal),
+                &Type::BOOL => row.try_get::<_, bool>(i).map(UntaggedValue::boolean),
                 // &Type::DATE | &Type::TIME | &Type::TIMESTAMP | &Type::TIMESTAMPTZ =>
-                &Type::BYTEA => row.try_get::<_, Vec<u8>>(i).map(Value::binary),
-                _ => Ok(Value::nothing()),
+                &Type::BYTEA => row.try_get::<_, Vec<u8>>(i).map(UntaggedValue::binary),
+                _ => Ok(UntaggedValue::nothing()),
             };
-            dict.insert(col.name(), opt_value.unwrap_or(Value::nothing()));
+            dict.insert_untagged(col.name(), opt_value.unwrap_or(UntaggedValue::nothing()));
         }
-        records.push(dict.into_tagged_value());
+        records.push(dict.into_value());
     }
     Ok(records)
 }
@@ -70,8 +73,8 @@ impl Plugin for Psql {
     fn config(&mut self) -> Result<Signature, ShellError> {
         Ok(Signature::build("psql")
             .desc("Execute PostgreSQL query.")
-            .required("conn", SyntaxShape::String)
-            .required("query", SyntaxShape::String)
+            .required("conn", SyntaxShape::String, "DB connection string")
+            .required("query", SyntaxShape::String, "SQL query")
             // .rest(SyntaxShape::String)
             .filter())
     }
@@ -79,28 +82,28 @@ impl Plugin for Psql {
     fn begin_filter(&mut self, call_info: CallInfo) -> Result<Vec<ReturnValue>, ShellError> {
         if let Some(args) = call_info.args.positional {
             match &args[0] {
-                Tagged {
-                    item: Value::Primitive(Primitive::String(s)),
+                Value {
+                    value: UntaggedValue::Primitive(Primitive::String(s)),
                     ..
                 } => {
                     self.conn = Some(s.clone());
                 }
                 _ => {
-                    return Err(ShellError::string(format!(
+                    return Err(ShellError::untagged_runtime_error(format!(
                         "Unrecognized type in params: {:?}",
                         args[0]
                     )))
                 }
             }
             match &args[1] {
-                Tagged {
-                    item: Value::Primitive(Primitive::String(s)),
+                Value {
+                    value: UntaggedValue::Primitive(Primitive::String(s)),
                     ..
                 } => {
                     self.query = Some(s.clone());
                 }
                 _ => {
-                    return Err(ShellError::string(format!(
+                    return Err(ShellError::untagged_runtime_error(format!(
                         "Unrecognized type in params: {:?}",
                         args[1]
                     )))
